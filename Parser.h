4d9d3682bec89b164d0be4ec1c5257b75f49a7f1
yotammanne <yotammanne@mail.tau.ac.il>
2018-05-20 11:32:38 +0300

    logging

--- Parser.h
+++ Parser.h
@@ -6,10 +6,35 @@
 #include <stdio.h>
 
 
-
+/**
+ *
+ * @param command
+ * @param size
+ * @return
+ */
 int getInput(char* command, int size);
+
+/**
+ *
+ * @param command
+ * @param parsedCommand
+ * @return
+ */
 int parseCommand(char*command, int* parsedCommand);
+
+/**
+ *
+ * @param word
+ * @return
+ */
 int parseSize(char * word);
+
+/**
+ *
+ * @param str
+ * @param size
+ * @return
+ */
 int parseInt(char* str, int size);
 
 #endif
